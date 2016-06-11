74596b434e0b76cb994bc468b07b576ba8e15ea3
Jeffrey Stanton <jffry@users.noreply.github.com>
2016-06-11 01:35:17 -0400

    Support `-D`, `-h`, and `-S`

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 'use strict';
 
 let args = require('./parse-args')(process.argv.slice(1));
+var chalk = require('chalk');
 let stdout_intercept = require('intercept-stdout');
 let stdout_rainbowize = require('./stdout/rainbowize');
 let stdout_bees = require('./stdout/bees');
@@ -12,6 +13,7 @@ let BYTES_WRITTEN = 0;
 stdout_intercept(function(text)
 {
   BYTES_WRITTEN += text.length;
+  if (args.S) return '';
   text = args.B ? stdout_bees.extraBees(text) : text;
   text = args.b ? stdout_bees.noBees(text) : text;
   text = args.f ? stdout_rainbowize(text) : text;
@@ -37,3 +39,36 @@ if (args.c) {
   console.log(args._.length);
 }
 
+//deprecated
+if (args.D) {
+  console.log(chalk.white.bold.bgRed(`
+************************************************
+**  WARNING: THE -D FLAG HAS BEEN DEPRECATED  **
+**  PLEASE RUN  man blerp  FOR DETAILS        **
+************************************************
+`));
+}
+
+
+
+
+
+//last thing
+if (args.h) {
+  const interval = 20;
+  let n = 0;
+  let next = 0;
+  while (true) {
+    if (Date.now() >= next) {
+      next = Date.now() + interval;
+      process.stdout.write('.');
+      n += 1;
+      if (n >= 64) {
+        process.stdout.write('\n');
+        n = 0;
+      }
+    }
+
+    //TODO: trap CTRL+C / CTRL+D and refuse to close
+  }
+}
